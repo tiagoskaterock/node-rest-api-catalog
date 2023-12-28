4c778349e529be4c02f5bab73c8f3b2edb27b162
tiagoskaterock <tiagolemespalhano@gmail.com>
2023-12-28 04:16:49 -0300

    17. Post, Put & Delete Requests

--- index.js
+++ index.js
@@ -34,24 +34,48 @@ app.get('/api/products/:_id', function(req, res, next) {
 		if(err) {
 			res.send(err)
 		}
-		console.log(doc)
 		res.json(doc)
 	})
 })
 
 // add product
 app.post('/api/products', function(req, res, next) {
-	res.send('Add product')
+	db.products.insert(req.body, function (err, doc) {
+		if(err) {
+			res.send(err)
+		}
+		res.json(doc)
+		console.log('Adding product')
+	})
 })
 
 // update product
-app.put('/api/products:id', function(req, res, next) {
-	res.send('Update product')
+app.put('/api/products/:_id', function(req, res, next) {
+	db.products.findAndModify({query: {_id: mongojs.ObjectId(req.params._id)},
+	update:{
+		$set:{
+			name: req.body.name,
+			category: req.body.category,
+			details: req.body.details
+		}
+	},
+	new: true }, function(err, doc) {
+		if(err) {
+			res.send(err)
+		}
+		console.log('Updating product')
+		res.json(doc)
+	})
 })
 
 // delete product
-app.delete('/api/products/:id', function(req, res, next) {
-	res.send('Delete product')
+app.delete('/api/products/:_id', function(req, res, next) {
+	db.products.remove({_id: mongojs.ObjectId(req.params._id)}, function(err, doc) {
+		if(err) {
+			res.status(500).send(err)			
+		}
+		res.status(200).send('Product deleted')
+	})	
 })
 
 app.listen(port, function() {
