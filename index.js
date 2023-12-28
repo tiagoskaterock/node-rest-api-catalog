934904cd9dd555b023e425f86e50e507020320dd
tiagoskaterock <tiagolemespalhano@gmail.com>
2023-12-28 02:02:27 -0300

    16. Fetching Data From The API

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongojs = require('mongojs')
 
+const db = mongojs('catalog', ['products'])
+
 const app = express()
 
 const port = 3000
 
 app.use(bodyParser.json())
+
+
 
 // home
 app.get('/', function(req, res, next) {
@@ -15,12 +19,24 @@ app.get('/', function(req, res, next) {
 
 // fetch all products
 app.get('/api/products', function(req, res, next) {
-	res.send('All products')
+	db.products.find(function (err, docs) {
+		if(err) {
+			res.send(err)
+		}
+		console.log('Products')
+		res.json(docs)
+	})
 })
 
 // fetch single product
-app.get('/api/products/:id', function(req, res, next) {
-	res.send('Single product ' + req.params.id)
+app.get('/api/products/:_id', function(req, res, next) {
+	db.products.findOne({ _id: mongojs.ObjectId(req.params._id)}, function (err, doc) {
+		if(err) {
+			res.send(err)
+		}
+		console.log(doc)
+		res.json(doc)
+	})
 })
 
 // add product
